d1e2c80940b589c1694ac3b2fe779b88865d8e61
TamPhilip <philip.tam.code@gmail.com>
2019-11-15 22:19:18 -0500

    added ios app

---
+++ Musical Journal/Musical Journal/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Musical Journal
+//
+//  Created by Philip Tam on 2019-11-15.
+//  Copyright © 2019 musicaljournal. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
